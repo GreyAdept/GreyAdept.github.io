917427391b19ea093d0ffbc0ddb70f248fbf7d76
Adalwolf <aatu.leppa11@gmail.com>
2025-01-23 13:48:42 +0200

    cloudflare_confiq

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://GreyAdept.github.io',
-  base: '/greyadept.github.io'
+  output: 'server',
+  //site: 'https://GreyAdept.github.io',
+  //base: '/greyadept.github.io',
+  adapter: cloudflare()
 });
