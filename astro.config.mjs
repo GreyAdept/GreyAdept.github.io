b9c473f5788cfb1f8fef4a2937384f8d08d96641
Adalwolf <aatu.leppa11@gmail.com>
2025-01-30 12:51:27 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,19 @@ import tailwind from '@astrojs/tailwind';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) =>
+        page !== 'https://tyhjyydenvalu.com/gallery_1513' &&
+        page !== 'https://tyhjyydenvalu.com/tekijat' &&
+        page !== 'https://tyhjyydenvalu.com/essee' ,
+    }),
+  ],
   output: 'server',
   //site: 'https://GreyAdept.github.io',
   //base: '/greyadept.github.io',
